87f5bd9fc62196a8a6bd85d822929bd1361e5d53
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-05-03 18:10:49 -0300

    Add files via upload

--- ReinoDosEnigmas/teste.py
+++ ReinoDosEnigmas/teste.py
@@ -1,4 +1,5 @@
 from Player import Player
+from funcoes import *
 
 BLUE  = "\033[1;34m"
 GREEN = "\033[0;32m"
@@ -11,7 +12,13 @@ playerClass = Player(player)
 
 
 
-def cidade():
-    print(f"\n\nSeguindo pela floresta {playerClass.name} consegue aos poucos ir percebendo que a vegetação ao seu redor estava mudando, até encontrar uma segunda trilha bem a sua frente, e para a sua surpresa ela aparentava ser bem utilizada.\n Seguindo ela, {playerClass.name} em pouco tempo já se deparava com os enormes muros de uma grande cidade. Adentrando-a, a primeira coisa que chamou a atenção foram os diversos tipos de comerciantes, guardas da cidade e a população reunida em um grande centro com uma praça.\n Desesperado e com medo {playerClass.name} vai atrás de uma ajuda o mais rápido possivel, onde acaba abordando um vendedor em sua barraca de legumes.\n\n {BLUE+'vendedor'+RESET} - Saldações viajante!, o que deseja comprar?\n\n {GREEN+playerClass.name+RESET} - Olá, acabo de chegar nesta cidade e gostaria de saber onde poderia me hospedar.\n\n {BLUE+'vendedor'+RESET} - Ora, a cidade está muito movimentada devido a grandiosa chegada do principe, o filho do rei finalmente retornou depois de uma de suas grandes cavalgadas. Sinto em lhe dizer viajante, más todas as hospedarias da cidade estão ocupadas, a festa será grandiosa!\n\n {GREEN+playerClass.name+RESET} - Não estava sabendo deste evento, estava passando apenas de viagem e iria embora logo ao amanhecer, o senhor conheceria alguem que pudesse me ceder um local para dormir?\n\n o vendedor encara {playerClass.name} da cabeça aos pés...\n\n {BLUE+'vendedor'+RESET} - Bem jovem... estou aqui apenas a trabalho, moro em uma pequena fazenda seguindo ao Norte.\n Entendo a sua situação, um dia eu já fui como voçe, apenas um viajante entre as cidades.\n caso o interesse, o Celeiro de minha fazenda talvez possa te abrigar por hoje.\n\n {GREEN+playerClass.name+RESET} - Claro Senhor! seria um favor enorme...\n")
+def caverna():
+    textoAnimado(f"\n{playerClass.name} decide ignorar a trilha e se esconder no lugar mais próximo possivel.\nDentro da caverna {playerClass.name} percebe que o ambiente um dia já havia sido uma grande mineração, porém hoje ela estava abandonada e com várias ferramentas antigas e carrinhos cheios de pedra por toda parte.\nExplorando mais o local em busca de uma saida, ao fundo de uma fenda, {playerClass.name} consegue vizualizar uma luz fraca de um lampião.\nChegando mais perto ainda, ele se depara com um Anão com uma enorme barba branca vestindo um macacão sujo e com uma picareta, inpecionando um pequeno cristal tranasparente. {playerClass.name} fica com medo mas percebe que o velho Anão minerador não poderia ser um problema maior do que o Javali do lado de fora da caverna, e resolve ir falar com ele...",0.01)
+    textoAnimado(f"\n\n{GREEN+playerClass.name+RESET} - O..Olá?...\n\n O Anão minerador olha para {playerClass.name} e da um pulo de susto...\n\n{BLUE+'Minerador'+RESET} - MEU DEUS!, quem lhe contou que eu estava aqui?? Por acaso te falaram alguma coisa sobre mim???\n\n{GREEN+playerClass.name+RESET} - Não não senhor..., eu só entrei aqui procurando um lugar para me esconder, lá fora tem um Javali que está me perseguindo\n\n{BLUE+'Minerador'+RESET} - Um Javali?, voce entrou nesta antiga mina abandonada só por causa de um Javali?\n\n{GREEN+playerClass.name+RESET} - Sim...\n\n{BLUE+'Minerador'+RESET} - hmm, bem, já que voce está por aqui, finja que nunca me viu, tenho muito trabalho por aqui.\n\n{GREEN+playerClass.name+RESET} - Más o que o senhor está fazendo? preciso de ajuda urgente!, meu tempo é curto e preciso encontrar um local para dormir.\n\n{BLUE+'Minerador'+RESET} - Voce fala demais para o meu gosto eim 'viajante'...\nAposto que foi andado pelos meus irmãos para me vijiar, más como esta caverna não é para amadores como voce, me ajude com uma duvida e eu penso se eu te mostro a saida.\n\n{playerClass.name} fica confuso más aceita o pedido, afinal, não teria como a situação ficar pior.\n\n{GREEN+playerClass.name+RESET} - Ok, eu ajudo.",0.01)
 
-cidade()
+caverna()
+
+
+
+#0.08
+
