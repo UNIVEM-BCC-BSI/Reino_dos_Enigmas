2325932bab47593658a00d3af6ee4d41234b8a7f
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-05-06 01:53:06 -0300

    Add files via upload

--- ReinoDosEnigmas/teste.py
+++ ReinoDosEnigmas/teste.py
@@ -1,7 +1,11 @@
 from src.Modelo.Perguntas import *
+from src.Modelo.Player import *
 from src.Config.funcoes import *
 
-PerguntaRio = Questao('Com quantos Paus se faz uma canoa?\n1- um\n2- dois\n2- tres\n','tres paus','3','marcenaria','como fazer uma canoa')
+nome = input('nome: ')
+jogador = Player(nome,'moedas',5)
+
+PerguntaRio = Questao('Com quantos Paus se faz uma canoa?\n1- um\n2- dois\n3- tres\n','tres paus','3','marcenaria','como fazer uma canoa')
 
 PerguntaRio.exibirEnunciado()
 resposta = input('Resposta: ')
@@ -10,3 +14,18 @@ PerguntaRio.verificarResposta(resposta)
 print('Relatorio')
 
 PerguntaRio.relatorio()
+
+
+
+
+
+
+if PerguntaRio.status == True:
+    print(f'\notimo {jogador.name}, a resposta esta correta, aqui está o seu premio\n')
+    jogador.adicionaItem('moedas',50)
+    print('moedas de ouro adicionadas na mochila')
+    jogador.exibeInventario()
+else:
+    print(f'puxa {jogador.name}, a resposta esta errada!\n')
+
+
